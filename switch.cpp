9605205acf8040537e3e5ad518dcfe1b187db8eb
Grant Hill <mooman72v2@gmail.com>
2015-04-24 00:53:39 -0600

    Allowed for global access to opcode constants.

--- switch.cpp
+++ switch.cpp
@@ -1,35 +1,34 @@
 #include "instruc.h"
 #include "Instructions.cpp"
 
+const int HALT = 0x0;
+const int NOP = 0x1;
+const int LD = 0x10;
+const int ST = 0x11;
+const int EM = 0x12;
+const int LDX = 0x18;
+const int STX = 0x19;
+const int EMX = 0x1a;
+const int ADD = 0x20;
+const int SUB = 0x21;
+const int CLR = 0x22;
+const int COM = 0x23;
+const int AND = 0x24;
+const int OR  = 0x25;
+const int XOR = 0x26;
+const int ADDX = 0x28;
+const int SUBX = 0x29;
+const int CLRX = 0x2a;
+const int J   = 0x30;
+const int JZ  = 0x31;
+const int JN  = 0x32;
+const int JP  = 0x33;
+
+
 void switchFunction( instruc instr_data );
-
-
+	
 void switchFunction( instruc instr_data )
 {
-	const int HALT = 0x0;
-	const int NOP = 0x1;
-	const int LD = 0x10;
-	const int ST = 0x11;
-	const int EM = 0x12;
-	const int LDX = 0x18;
-	const int STX = 0x19;
-	const int EMX = 0x1a;
-	const int ADD = 0x20;
-	const int SUB = 0x21;
-	const int CLR = 0x22;
-	const int COM = 0x23;
-	const int AND = 0x24;
-	const int OR  = 0x25;
-	const int XOR = 0x26;
-	const int ADDX = 0x28;
-	const int SUBX = 0x29;
-	const int CLRX = 0x2a;
-	const int J   = 0x30;
-	const int JZ  = 0x31;
-	const int JN  = 0x32;
-	const int JP  = 0x33;
-
-	
 
 	switch( instr_data.op )
 	{
