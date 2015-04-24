84356523d502a4bbd2650dfcdd5021584074f4c9
Grant Hill <mooman72v2@gmail.com>
2015-04-24 13:38:52 -0600

    Added cstdlib for exit(), library comments

--- instruc.h
+++ instruc.h
@@ -1,9 +1,10 @@
 #ifndef INSTRUC_H
 #define INSTRUC_H
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
+#include <iostream> //Printing trace
+#include <fstream> //To open the .obj file
+#include <string> //For passing strings to trace()
+#include <iomanip> //For std::hex, setw()
+#include <cstdlib> //for exit();
 
 using namespace std;
 
